1d61e9bbada5d7bb52ebdd86dfeff09e6c16443a
kajalyadav04 <kajalraj1520@gmail.com>
2025-03-30 23:22:31 +0530

    i love yiu

---
+++ tcs/removeVowel.py
@@ -0,0 +1,16 @@
+def removeVowels( self, s) :
+        # code here
+    v=0
+    c=0
+    result=[]
+    for ch in s:
+        if ch in "aeiouAEIOU":
+            
+            continue
+        else:
+            result.append(ch)
+    return "".join(result)
+s = 'kay'
+res = removeVowels(s)
+print(res)
+	
